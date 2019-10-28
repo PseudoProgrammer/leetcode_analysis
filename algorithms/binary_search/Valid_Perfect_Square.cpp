26a7c15eb940b7961bd2e7991463a18b711b1b07
PseudoProgrammer <custrb@163.com>
2019-10-28 15:22:18 +0800

    Create Valid_Perfect_Square.cpp

---
+++ algorithms/binary_search/Valid_Perfect_Square.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // method 1，暴力解法，从1到num遍历i，如果num / i == i && num % i == 0则迭代截止
+    // 时间复杂度o(n)，空间复杂度o(1) 
+    
+    // method 2, 二分查找，由于查找的代码是从1->num，解集是有序的，因而可以用二分查找来确定
+    // 时间复杂度o(log n), 空间复杂度o(1)
+    bool isPerfectSquare(int num) {
+        // 0, 处理特殊case
+        if (num == 1)return true;
+        // 1，初始化双指针
+        int i = 1;
+        // NOTE 1, 为了防止下面 (i + j) / 2对int范围越界，j != num，不影响结果
+        int j = num - 1;
+        // 2，迭代查找
+        while (j > i + 1) {
+            int mid = (i + j) / 2;
+            int div = num / mid;
+            if (div > mid)i = mid;
+            else if (div < mid)j = mid;
+            else if (num % mid != 0)i= mid;
+            // div == mid && num % mid == 0则mid * mid = num
+            else return true;
+        }
+        // 3，未找到，返回false
+        return false;
+    }
+};
